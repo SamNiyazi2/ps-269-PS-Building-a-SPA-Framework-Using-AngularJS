a88bfd926c9ab618c6a2c93e2fbef0876ab976a0
Sam Niyazi <sam2003@niyazi.com>
2021-01-23 12:14:59 -0600

    Completed M03-03.

---
+++ PS_269_Framework/ext-modules/psMenu/psMenuController.js
@@ -0,0 +1,11 @@
+﻿
+
+// 01/23/2021 12:08 pm - SSN - [20210123-1205] - [002] - M03-03 - Menu custom directives
+
+"use strict";
+
+angular.module("psMenu").controller("psMenuController", [ "$scope", function ($scope) {
+
+}]);
+
+
---
+++ PS_269_Framework/ext-modules/psMenu/psMenuDirective.js
@@ -0,0 +1,19 @@
+﻿
+// 01/23/2021 12:05 pm - SSN - [20210123-1205] - [001] - M03-03 - Menu custom directives
+
+"use strict";
+
+angular.module("psMenu").directive("psMenu", function () {
+
+    return {
+        // restrict: 'AE',
+        transclude: true,
+        templateUrl: 'ext-module/psMenu/psMenuTemplate.html',
+        controller: 'psMenuController',
+        link: function (scope, el, attr) {
+
+
+        }
+    };
+
+});
