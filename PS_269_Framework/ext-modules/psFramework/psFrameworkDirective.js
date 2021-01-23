54e7ba3dfd5fda10e0c5d22680f220cab57e4fe2
Sam Niyazi <sam2003@niyazi.com>
2021-01-23 10:34:16 -0600

    Completed M02-09.

--- PS_269_Framework/ext-modules/psFramework/psFrameworkDirective.js
+++ PS_269_Framework/ext-modules/psFramework/psFrameworkDirective.js
@@ -9,7 +9,10 @@ angular.module("psFramework").directive("psFramework", function () {
     return {
         transclude: true,
         scope: {
-
+            // 01/23/2021 10:30 am - SSN - [20210123-1024] - [001] - M02-09 - Passing a value to an isolated scope
+            title: '@',
+            subtitle: '@',
+            iconFile: '@',
         },
         controller: "psFrameworkController",
         templateUrl: "ext-modules/psFramework/psFrameworkTemplate.html"
