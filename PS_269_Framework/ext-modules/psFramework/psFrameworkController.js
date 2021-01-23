1e50b164aa03f4603678383071c1dafeff7cd859
Sam Niyazi <sam2003@niyazi.com>
2021-01-23 16:49:05 -0600

    Completed M03-10.

--- PS_269_Framework/ext-modules/psFramework/psFrameworkController.js
+++ PS_269_Framework/ext-modules/psFramework/psFrameworkController.js
@@ -5,6 +5,7 @@
 
 angular.module("psFramework").controller("psFrameworkController", ["$scope", function ($scope) {
 
+    $scope.isMenuButtonVisible = true;
 
     $scope.$on('ps-menu-item-selected-event', function (evt, data) {
 
