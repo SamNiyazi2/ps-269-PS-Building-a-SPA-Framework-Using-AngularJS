4895d375f90fcb0911b0083e06a8b5009541fed9
Sam Niyazi <sam2003@niyazi.com>
2021-01-23 11:39:03 -0600

    Completed M02..

---
+++ PS_269_Framework/app/appConfig.js
@@ -0,0 +1,34 @@
+﻿
+// 01/23/2021 10:42 am - SSN - [20210123-1034] - [001] - M02-10 - Catching exceptions
+
+'use strict';
+
+
+
+angular.module('app').config(function ($provide) {
+
+    // https://docs.angularjs.org/api/auto/service/$provide
+
+    $provide.decorator("$exceptionHandler", ["$delegate", function ($delegate) {
+
+        return function (exception, cause) {
+            console.log('20210123-1137: $exceptionHandler:');
+            console.log(exception);
+            console.log(cause1);
+
+            $delegate(exception, cause);
+
+            console.log('1-----------------------------------------------------');
+            console.log('2-----------------------------------------------------');
+            console.log('3-----------------------------------------------------');
+
+            alert(exception.message);
+
+
+        };
+
+    }]);
+
+
+});
+
