67e00b89cfe5747ffb6b1a80fe387ee3ee41fecf
Sam Niyazi <sam2003@niyazi.com>
2021-01-22 19:35:24 -0600

    Completed M02-05.

---
+++ PS_269_Framework/ext-modules/psFramework/psFrameworkController.js
@@ -0,0 +1,9 @@
+﻿
+// 01/22/2021 07:21 pm - SSN - [20210122-1915] - [002] -  M02-05 - Building our framework 
+
+"use strict";
+
+angular.module("psFramework").controller("psFrameworkController", ["$scope", function ($scope) {
+
+}]);
+
---
+++ PS_269_Framework/ext-modules/psFramework/psFrameworkDirective.js
@@ -0,0 +1,18 @@
+﻿
+// 01/22/2021 07:17 pm - SSN - [20210122-1915] - [001] -  M02-05 - Building our framework 
+
+"use strict";
+
+angular.module("psFramework").directive("psFramework", function () {
+
+
+    return {
+        transclude: true,
+        scope: {
+
+        },
+        controller: "psFrameworkController",
+        templateUrl: "ext-modules/psFramework/psFrameworkTemplate.html"
+    }
+
+});
