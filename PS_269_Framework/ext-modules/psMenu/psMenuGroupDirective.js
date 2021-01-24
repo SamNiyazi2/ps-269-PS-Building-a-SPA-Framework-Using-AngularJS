b52b4bb76d43edb25c7bc645e4bc49a6b502fa40
Sam Niyazi <sam2003@niyazi.com>
2021-01-24 12:47:43 -0600

    Completed M04-03.

---
+++ PS_269_Framework/ext-modules/psMenu/psMenuGroupDirective.js
@@ -0,0 +1,23 @@
+﻿
+// 01/24/2021 12:39 pm - SSN - [20210124-1238] - [001] - M04-03 - A custom directive for groups
+
+'use strict';
+
+angular.module('psMenu').directive('psMenuGroup', function () {
+
+    return {
+
+        require: '^psMenu',
+        transclude: true,
+        scope: {
+            label: '@',
+            icon: '@'
+        },
+        templateUrl: 'ext-modules/psMenu/psMenuGroupTemplate.html',
+        link: function (scope, el, attrs, ctrl) {
+
+
+        }
+    }
+
+});
