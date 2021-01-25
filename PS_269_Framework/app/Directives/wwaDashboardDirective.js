3e1047ef6ec7eecf1c8dbc83d0317a4a0d292ce3
Sam Niyazi <sam2003@niyazi.com>
2021-01-25 08:26:54 -0600

    Completed M05-08.

--- PS_269_Framework/app/Directives/wwaDashboardDirective.js
+++ PS_269_Framework/app/Directives/wwaDashboardDirective.js
@@ -9,7 +9,40 @@ angular.module("app").directive("wwaDashboard", [function () {
         scope: {
 
         },
-        template: '<ps-dashboard></ps-dashboard>'
+        template: '<ps-dashboard></ps-dashboard>',
+        link: function (scope) {
+
+            // 01/25/2021 08:13 am - SSN - [20210125-0809] - [001] - M05-08 - Dashboard options -- Gridster options
+
+            // https://github.com/manifestwebdesign/angular-gridster#configuration
+            scope.gridsterOpts = {
+                columns: 12,
+                margins: [20, 20],
+                outerMargin: false,
+                pushing: true,
+                floating: true,
+                swapping: false
+            };
+
+
+            scope.widgets = [
+                {
+                    sizeX: 3,
+                    sizeY: 3,
+                    row: 0,
+                    col: 0
+                }, {
+                    sizeX: 2,
+                    sizeY: 4,
+                    row: 0,
+                    col: 5
+                }
+
+
+            ];
+
+
+        }
     };
 
 }]);
--- PS_269_Framework/ext-modules/psDashboard/psDashboardDirective.js
+++ PS_269_Framework/ext-modules/psDashboard/psDashboardDirective.js
@@ -9,23 +9,6 @@ angular.module('psDashboard').directive('psDashboard', function () {
 
         link: function (scope) {
 
-            scope.widgets = [
-                {
-                    sizeX: 3,
-                    sizeY: 3,
-                    row: 0,
-                    col:0
-                }, {
-                    sizeX: 2,
-                    sizeY: 4,
-                    row: 0,
-                    col: 5
-                }
-
-
-            ];
-
-
         }
 
     };
