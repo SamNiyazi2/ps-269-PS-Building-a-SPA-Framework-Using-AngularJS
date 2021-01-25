855dbe140b6f44749bb68fe69cf9deff61b0abb1
Sam Niyazi <sam2003@niyazi.com>
2021-01-24 18:57:33 -0600

    Completed M05-03.

--- PS_269_Framework/app/appModule.js
+++ PS_269_Framework/app/appModule.js
@@ -1,5 +1,5 @@
 ﻿
 // 01/22/2021 07:07 pm - SSN - [20210122-1858] - [002] - M02-03 - Our AngularJS Modules
 
-angular.module("app", ["psFramework"]);
+angular.module("app", ["ngRoute" , "psFramework"]);
 
---
+++ PS_269_Framework/app/appRouteConfig.js
@@ -0,0 +1,44 @@
+﻿
+// 01/24/2021 06:46 pm - SSN - [20210124-1839] - [001] - M05-03 - Setting up routing
+
+"use strict";
+
+angular.module("app").config(["$routeProvider", function ($routeProvider) {
+
+    var routes = [
+
+        {
+            url: '/dashboard',
+            config: {
+                template: '<wwa-dashboard></wwwa-dashboard>'
+            }
+        },
+        {
+            url: '/locations',
+            config: {
+                template: '<wwa-locations></wwa-locations>'
+            }
+        },
+        {
+            url: '/guides',
+            config: {
+                template: '<wwa-guides></www-guides>'
+            }
+        }
+    ];
+
+
+    routes.forEach(function (route) {
+
+        $routeProvider.when(route.url, route.config);
+
+    });
+
+
+    $routeProvider.otherwise({ redirecTo: '/dashboard' });
+
+
+}]);
+
+
+
