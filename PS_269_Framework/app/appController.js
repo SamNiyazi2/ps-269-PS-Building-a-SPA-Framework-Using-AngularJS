89337acf0df7c5b6237bc6892c1c85451f079b97
Sam Niyazi <sam2003@niyazi.com>
2021-01-26 12:01:06 -0600

    Completed M07-06.

---
+++ PS_269_Framework/app/appController.js
@@ -0,0 +1,17 @@
+﻿
+// 01/26/2021 11:40 am - SSN - [20210126-1137] - [001] - M07-06 - A sign-in page using ngSwitch
+
+"use strict";
+
+angular.module("app").controller("appController", ["$scope", function ($scope) {
+
+    $scope.state = "unauthorized";
+
+    $scope.signIn = () => {
+
+        $scope.state = "authorized";
+    };
+
+
+}]);
+
